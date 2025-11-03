0f589b55491832ccecfcef1525eaf9ccfb165fb8
mohdthoriq <mohdthoriq@gmail.com>
2025-11-03 12:38:53 +0700

    feat: day4

--- App.tsx
+++ App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import { SafeAreaView, ScrollView, StyleSheet } from 'react-native';
-import HeroSection from './src/components/SimpleList';
-import ProfilCard from './src/components/FlatListExample';
-import LoginForm from './src/components/SectionListExample';
+import { SafeAreaView, StyleSheet, StatusBar } from 'react-native';
+import FlatListExample from './src/components/FlatListExample';
+import SectionListExample from './src/components/SectionListExample';
+import SimpleList from './src/components/SimpleList';
 
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView contentContainerStyle={styles.scroll}>
-        <HeroSection />
-        <ProfilCard />
-        <LoginForm />
-      </ScrollView>
+      <StatusBar barStyle="dark-content" backgroundColor="#f2f2f2" />
+      <FlatListExample />
+      {/* <SectionListExample/> */}
+      {/* <SimpleList /> */}
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: '#f2f2f2' },
-  scroll: { flexGrow: 1, padding: 20, gap: 20 },
+  container: {
+    flex: 1,
+    backgroundColor: '#f2f2f2',
+  },
 });
 
 export default App;
